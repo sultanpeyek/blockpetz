3cd27a8a1c955393e583284307977e97e61913af
sultanpeyek <sultanpeyek999@gmail.com>
2024-03-27 17:03:30 +0700

    use vercel flagging

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,6 @@
-export const BASE_URL =
-  process.env.VERCEL_ENV === "production"
-    ? "blockpetz.vercel.app"
-    : "http://localhost:3000";
+export const BASE_URL = process.env.VERCEL
+  ? "https://blockpetz.vercel.app"
+  : "http://localhost:3000";
 export const UPDATE_AUTHORITY = "PYKjwrSNWUmvy9VbSbYizi4xaC7MiCY1RNxq99UgU6X";
 export const EXP_PER_FEED = 100;
 export const EXP_PER_LEVEL = 500;
